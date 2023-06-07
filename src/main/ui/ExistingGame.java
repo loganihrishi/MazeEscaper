59dfb5bfdc1bcda6a92e809f926061ce29e4a597
Hrishi Logani <118128840+loganihrishi@users.noreply.github.com>
2023-06-07 13:28:08 +0530

    Added class to load ExistingData.java

---
+++ src/main/ui/ExistingGame.java
@@ -0,0 +1,16 @@
+package main.ui;
+
+import main.model.Maze;
+import main.model.Player;
+
+public class ExistingGame {
+    private final String filePath = "data/data.csv";
+    private final int width = 800;
+    private final int height = 600;
+    private Player player;
+    private Maze existingMaze;
+
+    public ExistingGame() {
+
+    }
+}
